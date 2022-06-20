063e5771473971901b0ecf0ce2c704b8302ab8e9
tarom0111 <77558188+tarom0111@users.noreply.github.com>
2022-06-21 01:55:09 +0700

    Add files via upload

---
+++ prohack.py
@@ -0,0 +1,19 @@
+import platform
+import os
+os.system('termux-setup-storage')
+#os.system('git pull')
+#try:os.system('mkdir /sdcard/PROHACK-DATA')
+#except:pass
+#try:os.system('mkdir /sdcard/PROHACK-DATA/OK')
+#except:pass
+#try:os.system('mkdir /sdcard/PROHACK-DATA/CP')
+#except:pass
+try:os.system('touch .prox.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+        __import__("Isom").menu()
+elif 'aarch' in arc:
+        __import__("Isom").menu()
+else:
+        exit(f' Unknow device machine {arc}')
